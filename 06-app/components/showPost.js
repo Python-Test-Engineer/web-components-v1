cd135aa1ee514c94fa3c3db67fc812c96a666acf
iwswordpress <iwswordpress@gmail.com>
2021-04-20 23:25:29 +0100

    06-app

--- 06-app/components/showPost.js
+++ 06-app/components/showPost.js
@@ -1,8 +1,6 @@
 // We can important site wide variables
 // either through an IMPORT or a CLASS
-import { SERVER, testFn } from '../00-ndc-global/global.js';
-
-console.log('[SERVER from import] ' + SERVER);
+import { SERVER, testFn } from './global.js';
 
 class ShowPost extends HTMLElement {
 	constructor() {
@@ -19,10 +17,10 @@ class ShowPost extends HTMLElement {
               border-radius: 10px;
               padding:20px;
               margin-bottom:20px;
-              background: var(--background-color-01, yellow);
+              background: var(--background, #ccc);
               font-size: var(--main-font-size, 20px);
               font-family: var(--main-font, inherit);
-              color: var(--main-text-color, red);
+              color: var(--main-font-color, black);
             }  
         </style>
         <!-- COMPONENT OUTPUT HERE -->
@@ -56,12 +54,8 @@ class ShowPost extends HTMLElement {
 		console.log('Component removed - disconnectedCallback()...');
 	}
 	_getPosts(postID) {
-		console.log(postID);
-		console.log('[SERVER] ' + SERVER);
-		//let url = SERVER + 'udemy/wp-json/wp/v2/posts/' + postID;
-
-		let url = '../_data/showPost' + postID + '.json';
-		console.log(url);
+		let url = SERVER + 'udemy/wp-json/wp/v2/posts/' + postID;
+		console.log('URL: ', url);
 		fetch(url)
 			.then(res => res.json())
 			.then(data => {
@@ -77,19 +71,6 @@ class ShowPost extends HTMLElement {
 				output += '</div>';
 				info.innerHTML += output;
 			});
-	}
-	// we can mutate component via one of its (public) methods
-	// we can also change the attribute within the component and this will re-render the component
-	// we can also use getters and setters to do this (be wary of infinite loop)
-	publicMethod(val) {
-		// alert('[FN] Public Method in Web Component called...' + val);
-		console.log('Public Method in Web Component called...[VALUE] ' + val);
-
-		// We can call methods in Web Component
-		// this._getPosts(40);
-
-		// We can also change attributes
-		// this.setAttribute('postid', 1142); // triggers attributeCallback lifecycle event
 	}
 }
 // We define custom component tag
