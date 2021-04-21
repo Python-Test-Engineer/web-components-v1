aaa90781c6c934eea2f12745a63d056f3e1e84c9
iwswordpress <iwswordpress@gmail.com>
2021-04-21 11:03:13 +0100

    10 NPM deploy added

---
+++ 10-npm-vanilla-class/index.js
@@ -0,0 +1,71 @@
+class ShowPost extends HTMLElement {
+	constructor() {
+		super();
+		console.log('CONSTRUCTOR...');
+		this.ID;
+		this.attachShadow({
+			mode: 'open',
+		});
+		this.shadowRoot.innerHTML = `
+        <style>
+            .card {
+              border: 4px solid #ccc;
+              border-radius: 10px;
+              padding:20px;
+              margin-bottom:20px;
+              background: var(--background, #ccc);
+              font-size: var(--main-font-size, 20px);
+              font-family: var(--main-font, inherit);
+              color: var(--main-font-color, black);
+            }  
+        </style>
+        <!-- COMPONENT OUTPUT HERE -->
+        <div id="info"></div>
+    `;
+	}
+	static get observedAttributes() {
+		return ['postid'];
+	}
+	attributeChangedCallback(name, oldValue, newValue) {
+		// this will fire initially as the element has no atrribute but is added when page runs
+		console.log('attribute has changed');
+		if (oldValue === newValue) {
+			return;
+		}
+		if (name === 'postid') {
+			console.log(name, oldValue, newValue);
+			this.ID = newValue;
+			this._getPosts(this.ID);
+		}
+	}
+	connectedCallback() {
+		// When component is added to DOM
+		console.log('connectedCallback fired...');
+	}
+	disconnectedCallback() {
+		// when component is removed from DOM
+		console.log('Component removed - disconnectedCallback()...');
+	}
+	_getPosts(postID) {
+		console.log(postID);
+
+		let url = 'https://wpjs.co.uk/wpb/wp-json/wp/v2/posts/' + postID;
+		console.log(url);
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				let output = '<br>';
+				// reference the output INFO element in this components Shadow DOM
+				const info = this.shadowRoot.querySelector('#info');
+				info.innerHTML = '<h3>URL: ' + url + '</h3>';
+				output += '<div class="card">';
+				output += 'PostID: <b>' + data.id + '</b><br> Title: <b>' + data.title.rendered + '</b><br> Author: ';
+				output += '<b>' + data.authorName.toUpperCase() + '</b><br>';
+				output += '<p>' + data.content.rendered + '</p>';
+				output += '</div>';
+				info.innerHTML += output;
+			});
+	}
+}
+customElements.define('show-post', ShowPost);
---
+++ 10-npm-vanilla-class/readme.md
@@ -0,0 +1,10 @@
+This takes a Vanilla Webn Component and publishes it to NPM.
+
+Deplpyed in iws-webcomponent-js.html, the Vanilla JS Web Component is used as is with:
+
+```
+class ShowPost extends HTMLElement {}
+customElements.define('show-post', ShowPost);
+```
+
+https://unpkg.com/iws-webcomponent-js@1.0.0/index.js
