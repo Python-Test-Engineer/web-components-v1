aab22c11de71f3258d58592020545f5f5e5e1895
iwswordpress <iwswordpress@gmail.com>
2021-04-25 09:22:47 +0100

    edit react example

--- 20-react-example/src/App.js
+++ 20-react-example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import logo from './logo.svg'; // Tell webpack this JS file uses this image
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,15 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<header className='App-header'>
-					<div>
-						<h1>IN APP</h1>
+					<div className='title'>
+						<h3>A Web Component in </h3>
+						<img src={logo} alt='Logo' />
+						<h3>app.js</h3>
 					</div>
 					<div className='web-component'>
+						<p>We can edit props postid to 1220, 1050, 955, 647</p>
 						<show-post postid='1220'></show-post>
+						<show-post postid='647'></show-post>
 					</div>
 				</header>
 			</div>
