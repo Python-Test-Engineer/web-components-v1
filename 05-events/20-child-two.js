3be4a0c48fefd7aa2e55345d20ce7fd67390adb7
iwswordpress <iwswordpress@gmail.com>
2021-06-15 05:04:32 +0100

    15JUN

--- 05-events/20-child-two.js
+++ 05-events/20-child-two.js
@@ -7,7 +7,7 @@ template2.innerHTML = `
         width: 500px;
         font-size:1.2rem;
         color:white;
-        background:orange;
+        background:purple;
         cursor:pointer;
       }
      
@@ -17,52 +17,52 @@ template2.innerHTML = `
 `;
 
 class ChildTwo extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({
+			mode: 'open',
+		});
+		// true means deep clone
+		this.shadowRoot.appendChild(template2.content.cloneNode(true));
+		//this.postListElement = this.shadowRoot.querySelector('section');
+	}
 
-  constructor() {
-    super();
-    this.attachShadow({
-      mode: 'open'
-    });
-    // true means deep clone
-    this.shadowRoot.appendChild(template2.content.cloneNode(true));
-    //this.postListElement = this.shadowRoot.querySelector('section');
-  }
+	connectedCallback() {
+		const btn = this.shadowRoot.getElementById('btn');
 
-  connectedCallback() {
-    const btn = this.shadowRoot.getElementById('btn');
+		btn.addEventListener('click', (e) => {
+			console.log('[CHILD TWO] +++++ CLICK START +++++');
+			console.log(e);
+			// we create an event and also send with it the postArray
+			this.dispatchEvent(
+				new CustomEvent('childTwoClick', {
+					detail:
+						'[TWO] Child Two can trigger an event and send data to parent that can be sent to Child One by Parent -> Child communication as per previous examples - attributes or Public Method. ',
+					bubbles: true,
+					composed: true,
+				}),
+			);
+			console.log('[CHILD TWO] +++++ CLICK END +++++ ');
+		});
+	}
+	attributeChangedCallback(attributeName, oldValue, newValue) {
+		if (oldValue === newValue) {
+			return;
+		}
+		if (attributeName === 'displaytext') {
+			const btn = this.shadowRoot.getElementById('btn');
+			btn.innerHTML = 'CHANGED BY COMPONENT ONE TO: ' + newValue;
+			//alert(btn.innerHTML);
+		}
+	}
 
-    btn.addEventListener('click', (e) => {
-      console.log("[CHILD TWO] +++++ CLICK START +++++");
-      console.log(e);
-      // we create an event and also send with it the postArray
-      this.dispatchEvent(new CustomEvent('childTwoClick', {
-        detail: '[TWO] Child Two can trigger an event and send data to parent that can be sent to Child One by Parent -> Child communication as per previous examples - attributes or Public Method. ',
-        bubbles: true,
-        composed: true
-      }));
-      console.log("[CHILD TWO] +++++ CLICK END +++++ ");
-
-    });
-  };
-  attributeChangedCallback(attributeName, oldValue, newValue) {
-    if (oldValue === newValue) {
-      return;
-    }
-    if (attributeName === 'displaytext') {
-      const btn = this.shadowRoot.getElementById('btn');
-      btn.innerHTML = 'CHANGED BY COMPONENT ONE TO: ' + newValue;
-      //alert(btn.innerHTML);
-    }
-  }
-
-  static get observedAttributes() {
-    return ['displaytext'];
-  }
-  publicMethod() {
-    //alert('\npublicMethod in COMPONENT TWO fired...\n\nwe can then do many things...');
-    console.log("Component TWO publicMethod fired...")
-  }
+	static get observedAttributes() {
+		return ['displaytext'];
+	}
+	publicMethod() {
+		//alert('\npublicMethod in COMPONENT TWO fired...\n\nwe can then do many things...');
+		console.log('Component TWO publicMethod fired...');
+	}
 }
-
 
 customElements.define('child-two', ChildTwo);
